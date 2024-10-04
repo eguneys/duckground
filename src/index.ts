b56c826060b5fc8390228a69ed459411e02179a0
Emre Guneyler <eguneys@gmail.com>
2024-10-04 22:12:13 +0300

    library mode

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { DuckBoard } from './DuckBoard'
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  build: {
+
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Duckground',
+      fileName: 'duckground',
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['solid-js', 'duckops']
+    }
+  }
 })
